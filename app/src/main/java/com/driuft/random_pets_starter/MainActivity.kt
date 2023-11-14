ab5fd7c809ce3e55ffb0ab58663b08008c460352
SebasHC7033 <sebastianhc7033@gmail.com>
2023-11-14 00:23:41 -0500

    user input added

--- app/src/main/java/com/driuft/random_pets_starter/MainActivity.kt
+++ app/src/main/java/com/driuft/random_pets_starter/MainActivity.kt
@@ -1,68 +1,261 @@
 package com.driuft.random_pets_starter
 
+
+import android.Manifest
+import java.time.LocalDateTime
+import java.util.Calendar
+import java.time.format.DateTimeFormatter
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Build
+import com.google.android.gms.location.LocationRequest
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.EditText
+import android.widget.Button
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
-import com.driuft.weatherapp.WeatherAdapter
-import okhttp3.Headers
-import org.json.JSONObject
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import okhttp3.Headers
+import org.json.JSONObject
+import com.driuft.weatherapp.WeatherAdapter
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.*
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+    private val REQUEST_LOCATION_PERMISSION = 1001
     private lateinit var weatherList: MutableList<Weather>
     private lateinit var rvWeather: RecyclerView
     lateinit var weatherJson: JSONObject
+    lateinit var cityInput: EditText
     var weatherCity: String = ""
     var weatherIcon = ""
     var weatherTemperature = 0.0
     var weatherTempMax = 0.0
     var weatherTempMin = 0.0
     var weatherTime = ""
+
     var weatherType = ""
 
 
     data class Weather(val time: String, val temperature: Double, val icon: String)
 
-    private fun getWeatherData(location: TextView, temperature: TextView, type: TextView, icon: ImageView, mmtemp: TextView) {
+    fun getDrawableResourceForWeatherIcon(iconCode: String): Int? {
+        return when (iconCode) {
+            "01d" -> R.drawable.day_clear
+            "01n" -> R.drawable.night_full_moon_clear
+            "02d" -> R.drawable.day_partial_cloud
+            "02n" -> R.drawable.night_full_moon_partial_cloud
+            "03d" -> R.drawable.cloudy
+            "03n" ->R.drawable.night_half_moon_partial_cloud
+            "04d" -> R.drawable.angry_clouds
+            "04n" -> R.drawable.night_full_moon_sleet
+            "09d" -> R.drawable.rain
+            "09n" -> R.drawable.rain
+            "10d" -> R.drawable.day_rain
+            "10n" ->R.drawable.night_half_moon_rain
+            "11d" -> R.drawable.day_rain_thunder
+            "11n" -> R.drawable.night_full_moon_rain_thunder
+            "13d" -> R.drawable.snow
+            "13n" ->R.drawable.snow
+
+
+            // ... map other icon codes ...
+            else -> null // A default icon
+        }
+    }
+
+    private fun hasLocationPermission():Boolean {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED
+    }
+    private fun requestLocationPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            REQUEST_LOCATION_PERMISSION
+        )
+    }
+
+
+    private fun startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null)
+    }
+
+    private fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+
+
+    private fun getWeatherData(cityLocation: TextView, temperature: TextView, type: TextView, icon: ImageView, mmtemp: TextView, userInput: String?) {
         val client = AsyncHttpClient()
-        val cityID = "5128581"
         val apiKey = "7dc1b7f05e79b0e2e9bb55d8e7a84e83" // Your API Key
+        val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
-        client["https://api.openweathermap.org/data/2.5/forecast?id=$cityID&appid=$apiKey&units=metric", object : JsonHttpResponseHandler() {
+        if (hasLocationPermission()) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return
+            }
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location: Location? ->
+                    location?.let {
+                        var latitude = it.latitude
+                        var longitude = it.longitude
+
+                        // Make the weather API request with latitude and longitude
+                        var currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey&units=metric"
+                        if (userInput != null){
+                            currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?id=$userInput&appid=$apiKey&units=metric"
+                        }
+                        client.get(
+                            currentWeatherUrl,
+                            object : JsonHttpResponseHandler() {
+                                @RequiresApi(Build.VERSION_CODES.O)
+                                override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
+                                    Log.d("Weather Success", "$json")
+
+                                    val dateView = findViewById<TextView>(R.id.currentDate)
+
+                                    val calendar = Calendar.getInstance()
+                                    val currentDateTime = LocalDateTime.now()
+                                    val formatter =
+                                        SimpleDateFormat("EEE, d, MMMM HH:mm", Locale.getDefault())
+                                    val formattedDateTime = formatter.format(calendar.time)
+
+                                    dateView.text = formattedDateTime
+
+
+                                    if (json.jsonObject.has("name")) {
+                                        // If the key exists, then retrieve its value
+                                        val cityName = json.jsonObject.getString("name")
+                                        Log.d("City Name", cityName)
+
+
+                                        weatherJson = json.jsonObject.getJSONObject("main")
+                                        weatherTempMax =
+                                            String.format("%.1f", weatherJson.getDouble("temp_max"))
+                                                .toDouble()
+                                        weatherTempMin =
+                                            String.format("%.1f", weatherJson.getDouble("temp_min"))
+                                                .toDouble()
+                                        weatherTemperature =
+                                            String.format("%.1f", weatherJson.getDouble("temp"))
+                                                .toDouble()
+                                        weatherType =
+                                            json.jsonObject.getJSONArray("weather").getJSONObject(0)
+                                                .getString("main")
+                                        weatherIcon =
+                                            json.jsonObject.getJSONArray("weather").getJSONObject(0)
+                                                .getString("icon")
+                                        val drawableId =
+                                            getDrawableResourceForWeatherIcon(weatherIcon)
+                                        val weatherTempCombined =
+                                            "$weatherTempMax°/$weatherTempMin°"
+
+                                        cityLocation.text = cityName
+                                        temperature.text = weatherTemperature.toString()
+                                        type.text = weatherType
+                                        mmtemp.text = weatherTempCombined
+                                        drawableId?.let {
+                                            icon.setImageResource(it)
+                                        } ?: kotlin.run {
+                                            Glide.with(icon)
+                                                .load("https://openweathermap.org/img/wn/${weatherIcon}.png")
+                                                .fitCenter()
+                                                .into(icon)
+                                        }
+
+                                        fetchForecastData(client,latitude,longitude,apiKey)
+
+                                    }
+
+                                }
+                                override fun onFailure(
+                                    statusCode: Int,
+                                    headers: Headers?,
+                                    errorResponse: String,
+                                    throwable: Throwable?
+                                ) {
+                                    Log.d("Weather Error", errorResponse)
+                                }
+                            }
+                        )
+                    }
+                }
+        } else {
+            requestLocationPermission()
+        }
+    }
+
+    private fun fetchForecastData(client: AsyncHttpClient, latitude: Double, longitude: Double, apiKey: String) {
+        val forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=$latitude&lon=$longitude&appid=$apiKey&units=metric"
+
+        client.get(forecastUrl, object : JsonHttpResponseHandler() {
             override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
-                Log.d("Weather Success", "$json")
-
-                weatherCity = "New York"
-                weatherJson = json.jsonObject.getJSONArray("list").getJSONObject(0).getJSONObject("main")
+                // Process forecast data
+                weatherJson =
+                    json.jsonObject.getJSONArray("list").getJSONObject(0).getJSONObject("main")
                 weatherTempMax = weatherJson.getDouble("temp_max")
                 weatherTempMin = weatherJson.getDouble("temp_min")
                 weatherTemperature = weatherJson.getDouble("temp")
-                weatherTime = json.jsonObject.getJSONArray("list").getJSONObject(0).getString("dt_txt")
-                weatherType = json.jsonObject.getJSONArray("list").getJSONObject(0).getJSONArray("weather").getJSONObject(0).getString("main")
-                weatherIcon = json.jsonObject.getJSONArray("list").getJSONObject(0).getJSONArray("weather").getJSONObject(0).getString("icon")
+                weatherTime =
+                    json.jsonObject.getJSONArray("list").getJSONObject(0).getString("dt_txt")
+                weatherType =
+                    json.jsonObject.getJSONArray("list").getJSONObject(0).getJSONArray("weather")
+                        .getJSONObject(0).getString("main")
+                weatherIcon = json.jsonObject.getJSONArray("list").getJSONObject(0).getJSONArray("weather")
+                        .getJSONObject(0).getString("icon")
+
                 val weatherTempCombined = "$weatherTempMax°/$weatherTempMin°"
 
 
-                location.text = weatherCity
-                temperature.text = weatherTemperature.toString()
-                type.text = weatherType
-                mmtemp.text = weatherTempCombined
-                Glide.with(icon)
-                    .load("https://openweathermap.org/img/wn/${weatherIcon}.png")
-                    .fitCenter()
-                    .into(icon)
-                for (i in 0 until 10){
+                for (i in 0 until 10) {
                     weatherJson = json.jsonObject.getJSONArray("list").getJSONObject(i).getJSONObject("main")
                     weatherTime = json.jsonObject.getJSONArray("list").getJSONObject(i).getString("dt_txt").removeRange(0, 11).removeRange(5, 8)
-                    weatherTemperature = weatherJson.getDouble("temp")
+                    weatherTemperature = String.format("%.1f",weatherJson.getDouble("temp")).toDouble()
                     weatherIcon = json.jsonObject.getJSONArray("list").getJSONObject(i).getJSONArray("weather").getJSONObject(0).getString("icon")
                     val weather = Weather(weatherTime, weatherTemperature, weatherIcon)
                     weatherList.add(weather)
@@ -72,16 +265,14 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
-            override fun onFailure(
-                statusCode: Int,
-                headers: Headers?,
-                errorResponse: String,
-                throwable: Throwable?
-            ) {
-                Log.d("Weather Error", errorResponse)
+            override fun onFailure(statusCode: Int, headers: Headers?, errorResponse: String, throwable: Throwable?) {
+                Log.d("Forecast Error", "Forecast fetch failed: $errorResponse")
             }
-        }]
+        })
     }
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -91,6 +282,9 @@ class MainActivity : AppCompatActivity() {
         val currentWeather = findViewById<TextView>(R.id.currentWeather)
         val weatherIcon = findViewById<ImageView>(R.id.weatherIcon)
         val minmaxTemp = findViewById<TextView>(R.id.mmTemp)
+        val date = findViewById<TextView>(R.id.currentDate)
+
+
 
 
         rvWeather = findViewById(R.id.weather_list)
@@ -99,9 +293,39 @@ class MainActivity : AppCompatActivity() {
         val layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         rvWeather.layoutManager = layoutManager
 
-        getWeatherData(cityLocation, currentTemp, currentWeather, weatherIcon, minmaxTemp)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        locationRequest = LocationRequest.create().apply {
+            interval = 10000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult?.lastLocation?.let { location ->
+                    // Handle the received location data here
+                    getWeatherData(cityLocation, currentTemp, currentWeather, weatherIcon, minmaxTemp, null)
+                }
+            }
+        }
+
+        if (hasLocationPermission()) {
+            startLocationUpdates()
+        } else {
+            requestLocationPermission()
+        }
 
 
+        getWeatherData(cityLocation, currentTemp, currentWeather, weatherIcon, minmaxTemp, null)
+
+
+        findViewById<Button>(R.id.button)
+            .setOnClickListener {
+                cityInput = findViewById(R.id.cityInput)
+                val input: String = cityInput.text.toString().trim().lowercase()
+                if (input == "newyork"){
+                    getWeatherData(cityLocation, currentTemp, currentWeather, weatherIcon, minmaxTemp, "5128581")
+                }
+            }
 
     }
 
--- app/src/main/java/com/driuft/random_pets_starter/WeatherAdapter.kt
+++ app/src/main/java/com/driuft/random_pets_starter/WeatherAdapter.kt
@@ -1,5 +1,6 @@
 package com.driuft.weatherapp
 
+import android.graphics.drawable.AdaptiveIconDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,9 +10,13 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.driuft.random_pets_starter.MainActivity
 import com.driuft.random_pets_starter.R
+import com.driuft.random_pets_starter.WeatherIconUtils
+import com.driuft.random_pets_starter.WeatherIconUtils.getDrawableResourceForWeatherIcon
+
 //import com.driuft.weatherapp.MainActivity.Weather
 
 class WeatherAdapter(private val weatherList: List<MainActivity.Weather>) :
+
     RecyclerView.Adapter<WeatherAdapter.ViewHolder>() {
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -30,14 +35,18 @@ class WeatherAdapter(private val weatherList: List<MainActivity.Weather>) :
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val weather = weatherList[position]
-        holder.nameTextView.text = weather.time
-        holder.temperatureTextView.text = "${weather.temperature}°C"
+            holder.temperatureTextView.text = "${weather.temperature}°C"
+            holder.nameTextView.text = weather.time // Make sure you format this as needed
 
+        val iconDrawableId = WeatherIconUtils.getDrawableResourceForWeatherIcon(weather.icon)
+        if (iconDrawableId != null) {
+            holder.iconImageView.setImageResource(iconDrawableId)
+        }
 
-        // Assuming you have a valid url for the weather icon
-        Glide.with(holder.itemView.context)
-            .load("https://openweathermap.org/img/wn/${weather.icon}.png")
-            .into(holder.iconImageView)
+//        Glide.with(holder.itemView.context)
+//            .load("https://openweathermap.org/img/wn/${weather.icon}.png")
+//            .into(holder.iconImageView)
+
     }
 
     override fun getItemCount(): Int {
